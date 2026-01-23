451644f23ff842be22bcc6936c7c36fa5941b36f
boldagechris <cgyldenkaerne@gmail.com>
2026-01-23 14:17:44 +0100

    Refactor Appointment models for detailed data structures
    Updated Appointment models to include detailed time, location, and performer information in line with sundhed.dk's JSON structure. 
    Was failing due to incorrect structure for startTimeDetailed and endTimeDetailed that were decalred as strings and not objects. 
    location and performer expanded fields (adresse, organisation, etc.) 
    Added  @JsonIgnoreProperties(ignoreUnknown = true) to ignore extra fields

--- src/main/kotlin/dhroxy/model/AppointmentModels.kt
+++ src/main/kotlin/dhroxy/model/AppointmentModels.kt
@@ -1,7 +1,9 @@
 package dhroxy.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class AppointmentsResponse(
     @JsonProperty("appointments")
     val appointments: List<AppointmentItem> = emptyList(),
@@ -15,6 +17,7 @@ data class AppointmentsResponse(
     val reference: String? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class AppointmentItem(
     @JsonProperty("appointmentColorClass")
     val appointmentColorClass: String? = null,
@@ -25,21 +28,81 @@ data class AppointmentItem(
     @JsonProperty("endTime")
     val endTime: String? = null,
     @JsonProperty("endTimeDetailed")
-    val endTimeDetailed: String? = null,
+    val endTimeDetailed: TimeDetailed? = null,
     @JsonProperty("endTimeNotDefined")
     val endTimeNotDefined: Boolean? = null,
     @JsonProperty("location")
-    val location: AppointmentLocation? = null,
+    val location: LocationDetailed? = null,
     @JsonProperty("patient")
     val patient: AppointmentPerson? = null,
     @JsonProperty("performer")
-    val performer: AppointmentPerson? = null,
+    val performer: PerformerDetailed? = null,
     @JsonProperty("startTime")
     val startTime: String? = null,
     @JsonProperty("startTimeDetailed")
-    val startTimeDetailed: String? = null,
+    val startTimeDetailed: TimeDetailed? = null,
     @JsonProperty("title")
     val title: String? = null
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class TimeDetailed(
+    @JsonProperty("weekNo")
+    val weekNo: Int? = null,
+    @JsonProperty("timeFormatted")
+    val timeFormatted: String? = null,
+    @JsonProperty("dateFormatted")
+    val dateFormatted: String? = null
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AddressDetailed(
+    @JsonProperty("street")
+    val street: String? = null,
+    @JsonProperty("postalCode")
+    val postalCode: String? = null,
+    @JsonProperty("city")
+    val city: String? = null,
+    @JsonProperty("country")
+    val country: String? = null,
+    @JsonProperty("formatted")
+    val formatted: String? = null
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class LocationDetailed(
+    @JsonProperty("organisation")
+    val organisation: String? = null,
+    @JsonProperty("address")
+    val address: AddressDetailed? = null,
+    @JsonProperty("unitType")
+    val unitType: String? = null,
+    @JsonProperty("ward")
+    val ward: String? = null,
+    @JsonProperty("phone")
+    val phone: String? = null,
+    @JsonProperty("sorId")
+    val sorId: String? = null,
+    @JsonProperty("addressPhoneFormatted")
+    val addressPhoneFormatted: String? = null
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class PerformerDetailed(
+    @JsonProperty("organisation")
+    val organisation: String? = null,
+    @JsonProperty("address")
+    val address: AddressDetailed? = null,
+    @JsonProperty("unitType")
+    val unitType: String? = null,
+    @JsonProperty("ward")
+    val ward: String? = null,
+    @JsonProperty("phone")
+    val phone: String? = null,
+    @JsonProperty("sorId")
+    val sorId: String? = null,
+    @JsonProperty("addressPhoneFormatted")
+    val addressPhoneFormatted: String? = null
 )
 
 data class AppointmentLocation(
@@ -60,4 +123,16 @@ data class AppointmentPerson(
     val givenName: String? = null,
     @JsonProperty("personIdentifier")
     val personIdentifier: String? = null
+)
+
+@com.fasterxml.jackson.annotation.JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
+data class AppointmentsRequest(
+    @JsonProperty("FromDate")
+    val fromDate: String? = null,
+    @JsonProperty("ToDate")
+    val toDate: String? = null,
+    @JsonProperty("Version")
+    val version: Int = 2,
+    @JsonProperty("WithPartials")
+    val withPartials: Boolean = true
 )
